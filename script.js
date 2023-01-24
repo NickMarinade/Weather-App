83d866dfa29dcdc563c034172c235a8f3eefcd9a
NickMarinade <59340748+NickMarinade@users.noreply.github.com>
2023-01-24 14:04:11 +0100

    logic to get weather by city name

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ let temperature = document.querySelector('.weatherTemperature');
 let weatherApiKey = '94a0709f1e11021571d0d86134cebd6d';
 let weatherBaseEndpoint = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid='+ weatherApiKey;
 
-searchInput.addEventListener('keydown', (e) => {
-    console.log(e);
+let getWeatherByCityName = async (city) => {
+    let endpoint = weatherBaseEndpoint + '&q=' + city;
+    let response = await fetch(endpoint);
+    let weather = await response.json();
+
+    return weather;
+}
+
+searchInput.addEventListener('keydown', async (e) => {
+    if (e.keyCode === 13) {
+        let weather = await getWeatherByCityName(searchInput.value);
+        console.log(weather);
+    }
 })
